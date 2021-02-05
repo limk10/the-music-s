207de4315255a5c25744c21123123fd7ccea4a09
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-02-05 01:41:54 -0400

    Page base de home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <>
+      <h1>SignUp</h1>
+      <h2>SignUp</h2>
+      <h3>SignUp</h3>
+      <h4>SignUp</h4>
+      <h5>SignUp</h5>
+      <h6>SignUp</h6>
+    </>
+  );
+}
+
+export default SignUp;
