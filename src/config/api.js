e481b3c2a9b5960518e847aba372e536aa8560da
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-02-08 01:30:04 -0400

    Retirado .env e criado configuração pertinentes a APi

---
+++ src/config/api.js
@@ -0,0 +1,8 @@
+// Last.FM config
+export const LAST_FM_API = "http://ws.audioscrobbler.com/2.0";
+export const LAST_FM_KEY = "0fd02292fa7e631662828dd025552e06";
+export const LAST_FM_SECRET = "0667a639d2f497d8580f8cd205df43a8";
+
+// Autentication config
+export const REQRES_API = "https://reqres.in/";
+export const TOKEN_KEY = "the-music-s.permission";
