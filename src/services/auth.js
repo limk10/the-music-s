4916dfd76df004c3eb979bdbdd01ba8ac9074095
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-02-08 01:40:39 -0400

    Configuração de auth, onde estão todos os metodos pertinentes ao auth (removeKey, geyKey, setKey)

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,6 @@
-const tokenName = process.env.REACT_APP_TOKEN_KEY;
+import { TOKEN_KEY } from "~/config/api";
+
+const tokenName = TOKEN_KEY;
 export const isAuthenticated = () => localStorage.getItem(tokenName);
 export const getToken = () => localStorage.getItem(tokenName);
 
@@ -7,6 +9,6 @@ export const signIn = async (token) => {
   window.location = "/home";
 };
 export const logout = async () => {
-  await localStorage.removeItem(tokenName);
+  localStorage.removeItem(tokenName);
   window.location = "/";
 };
