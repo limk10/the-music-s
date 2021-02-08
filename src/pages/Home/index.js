ccf94872b10a3886625efa59ec00f8be01773b81
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-02-08 01:43:13 -0400

    Home do usuario, colocado uma breve explicação de algumas das tecnologias usadas no desenvolvimento

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 
-function SignUp() {
+function Home() {
   return (
-    <>
-      <h1>SignUp</h1>
-      <h2>SignUp</h2>
-      <h3>SignUp</h3>
-      <h4>SignUp</h4>
-      <h5>SignUp</h5>
-      <h6>SignUp</h6>
-    </>
+    <div className="content">
+      <h6>Olá, Um breve resumo do sistema :)</h6>
+      <p>
+        O sistema é um &quot;buscador&quot; de artistas e álbuns, foi
+        desenvolvido usando a biblioteca React ❤, com as tecnologias, ESLint com
+        padrão standart para organização do código, Babel Root Import para
+        organização de importações, Redux Thunk para controle de estados, Cirrus
+        UI para Kit de UI, Axios para integração com os backends, implementado
+        tratamento de erros no interceptor, também foi feito controle de
+        permissões nas rotas, no login usado JWT fake com a api REQRES... entre
+        outras tecnologias.
+      </p>
+
+      <h5>Bom teste!</h5>
+    </div>
   );
 }
 
-export default SignUp;
+export default Home;
