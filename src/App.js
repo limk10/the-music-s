715a353e496543c17c235e083ef049c0a4633af8
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-02-08 01:36:31 -0400

    Feito metodo para buscar usuario logado, colocado componente de header do app e feito controle de de autenticação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "~/routes";
 
+import { isAuthenticated } from "~/services/auth";
+
+import actionsLoading from "~/actions/loading";
+import actionsUser from "~/actions/user";
+import { REQRES_API } from "~/config/api";
+import api from "~/services/api";
+
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    try {
+      dispatch(actionsLoading.handleLoading(true));
+      const { data } = await api.get(REQRES_API + "api/users/4");
+      const { data: result } = data;
+      dispatch(actionsUser.addUser(result));
+    } catch (error) {
+    } finally {
+      dispatch(actionsLoading.handleLoading(false));
+    }
+  };
+
+  if (!isAuthenticated())
+    return (
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    );
+
   return (
     <BrowserRouter>
-      <Routes />
+      <div className="grid grid-gap-2">
+        <div className="grid-c-12">
+          <Header />
+        </div>
+        <div className="grid-c-12 px-4">
+          <Routes />
+        </div>
+      </div>
+      {/* <div className="grid-c-12">
+        <Footer />
+      </div> */}
     </BrowserRouter>
   );
 };
